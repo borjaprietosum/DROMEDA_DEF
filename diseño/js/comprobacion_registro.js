710c61ea8a580cea44d854e88a12ed6e274b5fe7
BorjaPrieto <borjaprietoimpt@gmail.com>
2022-05-02 10:27:23 +0200

    Comprobación Datos Registro

--- diseño/js/comprobacion_registro.js
+++ diseño/js/comprobacion_registro.js
@@ -1,61 +1,127 @@
 
 function validacion_formulario() {
     //Declaramos estas variables con su respectivo valor
+    let isOkeynombre = true;
+    let isOkeyContrasenha = true;
+    let isOkeyRepContrasenha = true;
+    let isOkeyEmail = true;
     const nombreUsuario = $("#nombre_usuario").val();
     const email = $("#email").val();
     const contrasenha = $("#contrasenha").val();
     const rep_contrasenha = $("#rep_contrasenha").val();
     //Comprobamos que el nombre de usuario no está vacío
     if (nombreUsuario == "") {
-        alert("Por favor, escribe tu nombre de usuario.");
-        return false;
+        $('#nombre_usuario').removeClass('ok');
+        $('#nombre_usuario').addClass('error');
+        isOkeynombre=false;
+    }else{
+        $('#nombre_usuario').removeClass('error');
+        $('#nombre_usuario').addClass('ok');
+        isOkeynombre=true;
     }
     if (contrasenha.length != "") {
-        $('#contrasenha').each(function(e) {
-            var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-            var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
-            var enoughRegex = new RegExp("(?=.{6,}).*", "g");
-            if (false == enoughRegex.test($(this).val())) {
-                    $('#passstrength').html('Más caracteres');
-            } else if (strongRegex.test($(this).val())) {
-                    $('#passstrength').className = 'ok';
-                    $('#passstrength').html('Fuerte!');
-            } else if (mediumRegex.test($(this).val())) {
-                    $('#passstrength').className = 'alert';
-                    $('#passstrength').html('Media!');
-            } else {
-                    $('#passstrength').className = 'error';
-                    $('#passstrength').html('Débil!');
-            }
-            return true;
-       });
-    }else{
-        alert("Por favor, escribe una contraseña");
-        return false;
+        $('#contrasenha').removeClass('error');
+        $('#contrasenha').addClass('ok');
+        isOkeyContrasenha = true;
+    } else {
+        $('#contrasenha').addClass('error');
+        $('#contrasenha').removeClass('ok');
+        isOkeyContrasenha = false;
+        
     }
     if (rep_contrasenha != contrasenha) {
-        alert("Las contraseñas no son iguales")
+        $('#rep_contrasenha').addClass('error');
+        $('#rep_contrasenha').removeClass('ok');
+        isOkeyRepContrasenha=false;
+    }else{
+        $('#rep_contrasenha').addClass('ok');
+        $('#rep_contrasenha').removeClass('error');
+        isOkeyRepContrasenha=true;
     }
     //Comprobamos que el email no esté vacío y que cumple con los requisitos requeridos
     if (email != "") {
-        $('#email').each(function(e) { 
-            email_address = $(this); 
-            email_regex = /^[a-zA-Z0-9 ._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i; 
-            if(!email_regex.test(email_address.val())) { 
-                alert('Este correo electrónico no es válido'); 
-                e.preventDefault(); 
-                return false; 
-            } 
+        $('#email').each(function (e) {
+            email_address = $(this);
+            email_regex = /^[a-zA-Z0-9 ._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+            if (!email_regex.test(email_address.val())) {
+                $('#email').addClass('error');
+                $('#email').removeClass('ok');
+                isOkeyEmail=false;
+                e.preventDefault();
+            }else{
+                $('#email').addClass('ok');
+                $('#email').removeClass('error');
+                isOkeyEmail=true;
+            }
         });
+    } else {
+        $('#email').addClass('error');
+        $('#email').removeClass('ok');
+        isOkeyEmail=false;
+    }
+    $("#nombre_usuario").focusout(function () {
+        $('#nombre_usuario').removeClass('error');
+        $('#nombre_usuario').removeClass('ok');
+        if ($("#nombre_usuario").val().length < 1) {
+            $('#nombre_usuario').addClass('error');
+        } else {
+            $('#nombre_usuario').addClass('ok');
+            $('#nombre_usuario').removeClass('error');
+        }
+    })
+    $("#email").focusout(function () {
+        const email = $("#email").val();
+        $('#email').removeClass('error');
+        $('#email').removeClass('ok');
+        if (email != "") {
+            $('#email').each(function (e) {
+                $('#email').removeClass('ok');
+                email_address = $(this);
+                email_regex = /^[a-zA-Z0-9 ._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+                if (!email_regex.test(email_address.val())) {
+                    $('#email').addClass('error');
+                    $('#email').removeClass('ok');
+                    e.preventDefault();
+                } else {
+                    $('#email').addClass('ok');
+                }
+            });
+        } else {
+            $('#email').addClass('error');
+        }
+    })
+    $("#contrasenha").focusout(function () {
+        $('#contrasenha').removeClass('error');
+        $('#contrasenha').removeClass('ok');
+        console.log("Focusout")
+        if ($("#contrasenha").val().length < 1) {
+            $('#contrasenha').addClass('error');
+        } else {
+            $('#nombre').addClass('ok');
+            $('#nombre').removeClass('error');
+        }
+    })
+    $("#rep_contrasenha").focusout(function () {
+        $('#rep_contrasenha').removeClass('error');
+        $('#rep_contrasenha').removeClass('ok');
+        console.log("Focusout")
+        if ($("#rep_contrasenha").val()!=$("#contrasenha").val()) {
+            $('#rep_contrasenha').addClass('error');
+        } else {
+            $('#rep_contrasenha').addClass('ok');
+            $('#rep_contrasenha').removeClass('error');
+        }
+    })
+    if(isOkeyContrasenha==true && isOkeyEmail==true && isOkeyRepContrasenha==true && isOkeynombre==true){
+        console.log("datos enviados")
+        return true;
     }else{
-        alert("Por favor, escrib un correo electrónico");
         return false;
     }
-
 }
 $(document).ready(function () {
-$("#enviar").click(function (){
-    //Llamamos a la función
-    validacion_formulario();
-});
+    $("#enviar").click(function () {
+        //Llamamos a la función
+        validacion_formulario();
+    });
 });
